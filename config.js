f52923e14ef69c4c7579179df351576ac2412d62
Aden Lung <68804717+somenians@users.noreply.github.com>
2024-03-10 14:07:48 -0700

    Update config.js

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '8d09dc3e4c8c3bbca7ec67bb187550b3', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '37.540810',
+	defaultLongitude: '-122.033401',
 
 	// Autochange
 	autoChangeTheme: true,
